67859e670715e76cd8e62b3d0e1d6ccdd71fa6bc
Emilio Carrino <razorxan@gmail.com>
2020-06-10 22:15:04 +0200

    added port process.env

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import DreamSub from './DreamSub';
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 app.use(cors());
@@ -25,6 +27,6 @@ app.get('/:slug', async (req: express.Request, res: express.Response) => {
     res.json(result);
 });
 
-app.listen(3333, () => {
-    console.log('up and running on port 3333');
+app.listen(PORT, () => {
+    console.log(`up and running on port ${PORT}`);
 });
